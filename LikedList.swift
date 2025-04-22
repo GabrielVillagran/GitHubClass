6038c6681a58805fda6c994d466e99f5092a754b
Sebastian Gonzalez <sbt.goga@gmail.com>
2025-04-22 15:46:15 -0400

    Change after the mess from branch three

--- LikedList.swift
+++ LikedList.swift
@@ -54,4 +54,7 @@ func isPalindrome(_ text: String) -> Bool {
     return cleaned == String(cleaned.reversed())
 }
 
-
+func isNotPalindrome(_ text: String) -> Bool {
+    let cleaned = text.lowercased().filter { $0.isLetter }
+    return cleaned == !String(cleaned.reversed())
+}
